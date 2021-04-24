641ab9c70405a0320daaa63b8f63fbaf10ef6ed1
Samuel Jose Souza Oliveira <soliveira@mcity127.aquarius.cpqd.com.br>
2021-04-24 17:35:49 -0300

    Finalizando projeto chatbox com node e websocket

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { http } from "./http";
-import "./websocket/client"
+import "./websocket/client";
+import "./websocket/admin";
 
 //app.listen(3000, () => console.log("Server is running on port 3000")) //protocolo app do express para rotas do servidor
 http.listen(3000, () => console.log("WebSocket is running on port 3000")) //protocolo http do socket.io do WebSocket
